4954b3fd10713f12b9d3aff95d3ff5e3139d9bad
54younger <1831945484@qq.com>
2024-09-19 20:48:19 +0200

    finsish but dirty

--- python_program/MA2Files/MA2.py
+++ python_program/MA2Files/MA2.py
@@ -21,43 +21,73 @@ from MA2tokenizer import TokenizeWrapper
 
 
 class SyntaxError(Exception):
-    def __init__(self, arg):
-        self.arg = arg
-        super().__init__(self.arg)
+    def __init__(self, func_result):
+        self.func_result = func_result
+        super().__init__(self.func_result)
 
 
 def statement(wtok, variables):
     """ See syntax chart for statement"""
     result = assignment(wtok, variables)
+    if wtok.is_comment():
+        raise SyntaxError("Unexpected comment")
     return result
 
 
 def assignment(wtok, variables):
     """ See syntax chart for assignment"""
     result = expression(wtok, variables)
+
+    if wtok.get_current() == '=':
+        wtok.next()
+        if wtok.is_name():
+            variables[wtok.get_current()] = result
+            wtok.next()
+        else:
+            raise SyntaxError(" Expected variable after '='")
+        
+    
     return result
 
 
 def expression(wtok, variables):
     """ See syntax chart for expression"""
     result = term(wtok, variables)
+
     while wtok.get_current() == '+':
         wtok.next()
         result = result + term(wtok, variables)
+
+    while wtok.get_current() == '-':
+        wtok.next()
+        result = result - term(wtok, variables)
+
     return result
 
 
 def term(wtok, variables):
     """ See syntax chart for term"""
     result = factor(wtok, variables)
+
+
     while wtok.get_current() == '*': 
         wtok.next()
         result = result * factor(wtok, variables)
+
+    while wtok.get_current() == '/': 
+        wtok.next()
+        divisor = factor(wtok, variables)
+        if divisor == 0:
+            raise SyntaxError("Division by zero")
+        result = result / divisor
+
     return result
 
 
 def factor(wtok, variables):
     """ See syntax chart for factor"""
+    expected_functions = ['sin', 'cos', 'exp', 'log', 'fac']
+
     if wtok.get_current() == '(':
         wtok.next()
         result = assignment(wtok, variables)
@@ -70,9 +100,83 @@ def factor(wtok, variables):
         result = float(wtok.get_current())
         wtok.next()
 
+    elif wtok.get_current() in expected_functions:
+        func = wtok.get_current()
+        wtok.next()
+
+        if wtok.get_current() != '(':
+            raise SyntaxError("Expected '('")
+        
+        wtok.next()
+        func_result = assignment(wtok, variables)
+
+        if wtok.get_current() != ')':
+            raise SyntaxError("Expected ')'")
+        
+        wtok.next()
+        if func == 'sin':
+            result = math.sin(func_result)
+        elif func == 'cos':
+            result = math.cos(func_result)
+        elif func == 'exp':
+            result = math.exp(func_result)
+        elif func == 'log':
+            result = math.log(func_result)
+        elif func == 'fac':
+            if not func_result.is_integer() or func_result < 0:
+                raise SyntaxError("Expected nonnegative integer")
+            result = math.factorial(int(func_result))
+        else:
+            raise SyntaxError("Unknown function")
+        
+    elif wtok.get_current() == 'sum':
+
+        wtok.next()
+        if wtok.get_current() != '(':
+            raise SyntaxError("Expected '('")
+        result = 0
+        while 1:
+            wtok.next()
+            num = assignment(wtok, variables)
+            result += num
+            if wtok.get_current() != ',':
+                break
+
+        if wtok.get_current() != ')':
+            raise SyntaxError("Expected ')'")
+        wtok.next()
+    
+    elif wtok.get_current() == 'max':
+        wtok.next()
+        if wtok.get_current() != '(':
+            raise SyntaxError("Expected '('")
+        wtok.next()
+        max_val = assignment(wtok, variables)
+        while wtok.get_current() == ',':
+            wtok.next()
+            num = assignment(wtok, variables)
+            if num > max_val:
+                max_val = num
+            #wtok.next()
+        if wtok.get_current() != ')':
+            raise SyntaxError("Expected ')'")
+        wtok.next()
+        result = max_val
+
+    elif wtok.is_name():
+        if wtok.get_current() in variables:
+            result = variables[wtok.get_current()]
+            wtok.next()
+        else:
+            raise SyntaxError(f" Undefined variable: '{wtok.get_current()}'")
+    
+    elif wtok.get_current() == '-':
+        wtok.next()
+        result = -factor(wtok, variables)
+
     else:
         raise SyntaxError(
-            "Expected number or '('")  
+            "Expected number, name or '(' or function")  
     return result
 
 
@@ -101,12 +205,17 @@ def main():
     while True:
         if lines_from_file:
             line = lines_from_file.pop(0).strip()
-            print('init  :', line)
+            print('\ninit  :', line)
         else:
             line = input('\nInput : ')
         if line == '' or line[0]=='#':
             continue
         wtok = TokenizeWrapper(line)
+
+        if wtok.get_current() == 'vars':
+            for key, value in variables.items():
+                print(f'  {key} = {value}')
+            continue
 
         if wtok.get_current() == 'quit':
             print('Bye')
